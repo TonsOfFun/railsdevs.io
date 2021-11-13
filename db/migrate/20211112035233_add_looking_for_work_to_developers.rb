87e9666bce3b373672c43b9b1bbfe728c4f822ad
Joe Masilotti <joe@masilotti.com>
2021-11-13 09:31:27 -0800

    Perform migration with fewer queries
    Co-authored-by: Dave Gudge <dave@gudge.uk>

--- db/migrate/20211112035233_add_looking_for_work_to_developers.rb
+++ db/migrate/20211112035233_add_looking_for_work_to_developers.rb
@@ -1,11 +1,8 @@
 class AddLookingForWorkToDevelopers < ActiveRecord::Migration[7.0]
   def up
-    add_column :developers, :looking_for_work, :boolean
+    add_column :developers, :looking_for_work, :boolean, null: false, default: true
 
-    Developer.where("available_on < ?", Date.today + 1.year).update_all(looking_for_work: true)
-    Developer.where(looking_for_work: nil).update_all(looking_for_work: false)
-
-    change_column :developers, :looking_for_work, :boolean, null: false, default: true
+    Developer.where("available_on >= ?", Date.today + 1.year).update_all(looking_for_work: false)
   end
 
   def down
